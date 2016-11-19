446914102f5925d0727d8c1ff432f0860f68bc34
balajisankar1 <balajisankar1@gmail.com>
2016-11-19 15:03:29 -0500

    server can serach now and returns a json string response

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
-
+var balajiProducts = require('./mock_balaji.json');
+var leslieProducts = require('./mock_leslie.json');
 
 // test response to ensure that the server is live
 app.get('/', function(req, res){
 	res.send('Server is live');
+});
+
+app.get('/search/:str', function(req,res) {
+	retobjects = [];
+	for (obj in balajiProducts['data']) {
+		if (balajiProducts['data'][obj]['name'].toLowerCase().indexOf(req.params.str.toLowerCase()) > -1) {
+			retobjects.push(balajiProducts['data'][obj]);
+		}
+	}
+
+	for (obj in leslieProducts['data']) {
+		if (leslieProducts['data'][obj]['name'].toLowerCase().indexOf(req.params.str.toLowerCase()) > -1) {
+			retobjects.push(leslieProducts['data'][obj]);
+		}
+	}
+
+	res.send(JSON.stringify(retobjects));
 });
 
 app.listen(8080);
